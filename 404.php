60259e92213a06dcd5bcca15a4728553b563bd42
Kabo90 <84939836+Kabo90@users.noreply.github.com>
2021-05-27 16:42:03 -0700

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,42 @@
+<?php
+
+get_header();
+
+
+?>
+
+
+
+
+<div class="wraper">
+
+
+
+
+<div class="main">
+
+
+<!--sidebar-->
+<?php 
+get_sidebar(); 
+?>
+
+
+
+
+<!--posts-->
+<center><h2>Page Not Found</h2></center>
+
+
+</div>
+
+
+</div>
+
+<?php
+
+get_footer();
+
+
+?>
+
---
+++ footer.php
@@ -0,0 +1,28 @@
+<div class="footer">
+
+<div class="widgets">
+
+<?php
+dynamic_sidebar('footer-1');
+?>
+
+</div>
+
+<div class="copyRights">
+<center><h5><a target="_blank" href="https://mkabo.com">Designed By MKabo</a></h5></center>
+<center><h5>Copyright © 2021 YourWebsiteName. All Rights Reserved.</h5></center>
+</div>
+</div>
+
+
+
+<?php
+
+wp_footer();
+
+
+?>
+
+
+</body>
+</html>
